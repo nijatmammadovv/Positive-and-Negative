37aeba6fcc5728bee353f8f174df216f4f686285
nijatmammadovv <tu7q78xlv@code.edu.az>
2022-03-10 11:10:08 +0400

    Add project files.

---
+++ Positive and Negative/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Positive_and_negative
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number > 0)
+            {
+                Console.WriteLine("Number is positive");
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine("Number is negative");
+            }
+            else
+            {
+                Console.WriteLine("Number is zero");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
